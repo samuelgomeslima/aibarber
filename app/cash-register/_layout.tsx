73bafdceba05ef4c04a0e5224af9cfc4dd957a21
Samuel Gomes Lima <32044157+samuelgomeslima@users.noreply.github.com>
2025-10-23 09:40:16 -0300

    Fix cash register layout to use Slot

--- app/cash-register/_layout.tsx
+++ app/cash-register/_layout.tsx
@@ -1,15 +1,6 @@
 import React from "react";
-import { Tabs } from "expo-router";
+import { Slot } from "expo-router";
 
 export default function CashRegisterTabsLayout(): React.ReactElement {
-  return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBar: () => null,
-      }}
-    >
-      <Tabs.Screen name="index" options={{ title: "Cash register" }} />
-    </Tabs>
-  );
+  return <Slot />;
 }
