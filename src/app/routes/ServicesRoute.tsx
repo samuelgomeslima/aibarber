7c278dda869937aac51858750720e19d4ee2b6e5
Samuel Gomes Lima <32044157+samuelgomeslima@users.noreply.github.com>
2025-10-24 23:29:25 -0300

    Add services management route to TanStack navigation

---
+++ src/app/routes/ServicesRoute.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Services from "../../../app/services";
+
+export function ServicesRoute(): React.ReactElement {
+  return <Services />;
+}
+
+export default ServicesRoute;
